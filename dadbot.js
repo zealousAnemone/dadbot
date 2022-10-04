002ec271b8d1a3d9533f4a56048a27ff978aebb1
Veronica Stork <zealous.anemone@gmail.com>
2022-10-04 18:08:09 -0400

    First commit.{ :exit

---
+++ dadbot.js
@@ -0,0 +1,29 @@
+const Snoowrap = require('snoowrap');
+const { CommentStream } = require('snoostorm');
+require('dotenv').config();
+
+const client = new Snoowrap({
+  userAgent: process.env.USER_AGENT,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+});
+
+const comments = new CommentStream(client, {
+  subreddit: 'jokes',
+  limit: 25,
+});
+
+comments.on('item', (item) => {
+  const arr = item.body.split(' ');
+  if (arr[0] === "I'm" && arr.length === 2) {
+    let word = arr[1];
+    if (word.search(/[^a-z]$/)) {
+      word = word.slice(0, word.length - 1);
+    }
+    item.reply(`Hi ${word}, I'm dad!`);
+  } else {
+    return false;
+  }
+});
